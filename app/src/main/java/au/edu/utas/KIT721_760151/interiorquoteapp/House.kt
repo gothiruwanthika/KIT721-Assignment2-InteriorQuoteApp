82d5e51e0d4e2496586b1026c4ef00b74f60683f
Gothami Jayasingha <ruwanthi.gothami123@gmail.com>
2026-04-06 09:12:20 +1000

    Stage 2: Create basic house list empty state screen

---
+++ app/src/main/java/au/edu/utas/KIT721_760151/interiorquoteapp/House.kt
@@ -0,0 +1,12 @@
+package au.edu.utas.KIT721_760151.interiorquoteapp
+
+data class House(
+    var id: String = "",
+    var customerName: String = "",
+    var contactNumber: String = "",
+    var addressLine1: String = "",
+    var addressLine2: String = "",
+    var city: String = "",
+    var postalCode: String = "",
+    var email: String = ""
+)
--- app/src/main/java/au/edu/utas/KIT721_760151/interiorquoteapp/MainActivity.kt
+++ app/src/main/java/au/edu/utas/KIT721_760151/interiorquoteapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package au.edu.utas.KIT721_760151.interiorquoteapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import au.edu.utas.KIT721_760151.interiorquoteapp.databinding.ActivityMainBinding
 
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         ui = ActivityMainBinding.inflate(layoutInflater)
         setContentView(ui.root)
+
+        ui.fabNewHouse.setOnClickListener {
+            Toast.makeText(this, "Add House clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
